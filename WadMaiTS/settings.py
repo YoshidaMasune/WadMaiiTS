b1c258a702e0dadf72d1a654ece2cd5f229fe6f8
MunkNa <samaneranateechai@gmailcom>
2022-03-22 12:02:36 +0700

    setting database

--- WadMaiTS/settings.py
+++ WadMaiTS/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'WadMaiTS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wadmaits',
+        'USER': 'root',
+        'PASWORD': '',
+
     }
 }
 
