5e9d7afe6b60068258c75dd8800cc097b7490f9c
MunkNa <samaneranateechai@gmailcom>
2022-03-22 13:19:41 +0700

    setup overall

--- WadMaiTS/settings.py
+++ WadMaiTS/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'WadMaiTS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ moreapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'login.html')
+
+def createuser(request):
+    return render(request, 'register.html')
